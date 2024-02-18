4d4f68c5cd44ccfcdb6812155b74bf436e4989c4
liuyang <liuyangyangyang1992@gmail.com>
2024-02-18 21:51:54 +1100

    object

---
+++ Object.js
@@ -0,0 +1,9 @@
+let obj = {
+  firstname: 'yang',
+  isInstructor: true,
+  favioriteNumber: [1, 2, 4]
+}
+
+Object.keys(obj);
+// console.log(Object.entries(obj));
+console.log(obj.hasOwnProperty('firstname'))
