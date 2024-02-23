acf71f48999617400a6a12d8c34d33dc9ef3f415
刘洋 <liuyang@s-MacBook-Pro.local>
2024-02-23 12:14:46 +1100

    update selectionSort

--- selectionSort.js
+++ selectionSort.js
@@ -17,4 +17,23 @@ function selectionSort(arr) {
     return arr;
 }
 
-console.log(selectionSort([34,22,10,29,0,2,13]))
+// console.log(selectionSort([34,22,10,29,0,2,13]))
+
+// solution 2
+function newSelectionSort(arr) {
+    const swap = (arr, idx1, idx2) => 
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+    for(let i = 0; i< arr.length; i++) {
+        let loweset = i;
+        for (let j = i + 1; j< arr.length; j++) {
+            if (arr[loweset] > arr[j]) {
+                loweset = j;
+            }
+        }
+        if (i !== loweset) swap(arr, i, loweset)
+    }
+    return arr;
+}
+
+console.log(newSelectionSort([34,22,10,29,0,2,13]));
