c93fa97e6ccb11ee2c172d350b943fee96f17b9c
刘洋 <liuyang@s-MacBook-Pro.local>
2024-02-23 11:02:28 +1100

    update for bubblesort

--- bubbleSort.js
+++ bubbleSort.js
@@ -28,19 +28,36 @@ function compareByLen(str1, str2) {
 //   }
 
   // ES2015 Version
-function bubbleSort(arr) {
-    const swap = (arr, idx1, idx2) => {
-      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-    };
+// function bubbleSort(arr) {
+//     const swap = (arr, idx1, idx2) => {
+//       [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//     };
   
-    for (let i = arr.length; i > 0; i--) {
-      for (let j = 0; j < i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          swap(arr, j, j + 1);
+//     for (let i = arr.length; i > 0; i--) {
+//       for (let j = 0; j < i - 1; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//           swap(arr, j, j + 1);
+//         }
+//       }
+//     }
+//     return arr;
+//   }
+
+
+  function bubbleSort(arr) {
+    let noSwaps;
+    for (let i = arr.length; i>0; i--) {
+        noSwaps = true;
+        for(let j = 0; j<i -1; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1]
+                arr[j+1] = temp;
+                noSwaps = false
+            }
         }
-      }
+        if(noSwaps) break;
     }
     return arr;
   }
-  
 bubbleSort([37,45,29,8])
