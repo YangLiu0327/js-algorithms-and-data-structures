8f93bb0b590e450cbe2aeeb1741b385211216daa
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-02-28 10:09:03 +1100

    get key and values for hashtable

--- hashTable.js
+++ hashTable.js
@@ -56,6 +56,32 @@ class HashTable {
         }
         return undefined;
       }
+      values(){
+        let valuesArr = [];
+        for(let i =0; i<this.keyMap.length; i++) {
+            if(this.keyMap[i]){
+                for(let j =0; j<this.keyMap[i].length; j++) {
+                    if(!valuesArr.includes(this.keyMap[i][j][1])){
+                        valuesArr.push(this.keyMap[i][j][1])
+                    }
+                }
+            }
+        }
+        return valuesArr;
+      }
+      keys(){
+        let keysArr = [];
+        for(let i =0; i<this.keyMap.length; i++) {
+            if(this.keyMap[i]){
+                for(let j =0; j<this.keyMap[i].length; j++) {
+                    if(!keysArr.includes(this.keyMap[i][j][0])){
+                        keysArr.push(this.keyMap[i][j][0])
+                    }
+                }
+            }
+        }
+        return keysArr;
+      }
 }
 let ht = new HashTable(17);
 
@@ -67,4 +93,5 @@ ht.set("lightcoral","#F08080")
 ht.set("mediumvioletred","#C71585")
 ht.set("plum","#DDA0DD")
 
-console.log(ht.get("mediumvioletred"))
+// console.log(ht.get("mediumvioletred"))
+console.log(ht.keys())
