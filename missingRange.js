db2193804c891c3fcd859d137f3f41acb1be04ef
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-05-06 22:58:27 +1000

    update happy number

--- missingRange.js
+++ missingRange.js
@@ -22,4 +22,47 @@ function missingRange(nums, lower, upper) {
 }
 // [ 2, 4, 51, 76 ]
 // [[2,2],[4,49],[51,74],[76,99]]
-console.log(missingRange([0,1,3,50,75], -2, 99))
+// console.log(missingRange([0,1,3,50,75], -2, 99))
+
+function isHappy(n) {
+    let seen = new Set();
+    seen.add(n);
+
+    while(true) {
+        let currenSum = 0;
+        while(n >=10) {
+            currenSum += Math.pow(n % 10, 2); //　计算个位数的平方和
+            n = Math.floor(n / 10); // 向下取整
+        }
+        currenSum += Math.pow(n, 2) 
+        if (currenSum === 1) {
+            return true;
+          } else if (seen.has(currenSum)) {
+            return false;
+          }
+      
+          n = currenSum;
+          seen.add(n);
+    }
+}
+// console.log(isHappy(19))
+
+
+function addString (num1, num2) {
+ let arr1 = num1.split('').reverse();
+ let arr2 = num2.split('').reverse();
+
+ let result = [];
+ let carry  = 0;
+ for(let i =0; i<Math.max(num1.length, num2.length); i++) {
+    let sum = (parseInt(arr1[i]) || 0) + (parseInt(arr2[i]) || 0)  + carry;
+    result.push(sum % 10);
+    carry = Math.floor(sum / 10);
+ }
+ if(carry > 0) {
+    result.push(carry)
+ }
+ return result.reverse().join('')
+}
+
+// console.log(addString('1223', '555'))
