9af3de807d26d4c3e508a202f34085dc6ab6fe11
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-05-29 11:38:05 +1000

    update

--- SlidingWindow/findLongestSubstring.js
+++ SlidingWindow/findLongestSubstring.js
@@ -12,36 +12,36 @@
 
 function findLongestSubstring(str) {
   // 初始化最长子串长度为0，起始指针为0，字符索引映射对象
+  let left = 0;
   let longestSubstringLength = 0;
-  let start = 0;
-  let charIndexMap = {};
+  let lastSeen= {};
 
   // 遍历字符串
-  for (let end = 0; end < str.length; end++) {
-    const char = str[end];
-
+  for (let right = 0; right < str.length; right++) {
+    const char = str[right];
     // 如果字符重复且其上一次出现的位置在当前子串内
-    if (charIndexMap[char] >= start) {
+
+    if (lastSeen[char] >= left) {
       // 更新起始指针为重复字符的下一个位置
-      start = charIndexMap[char] + 1;
+      left = lastSeen[char] + 1;
     }
 
     // 更新字符索引映射
-    charIndexMap[char] = end;
+    lastSeen[char] = right;
 
     // 计算当前子串长度，并更新最长子串长度
-    longestSubstringLength = Math.max(longestSubstringLength, end - start + 1);
+    longestSubstringLength = Math.max(longestSubstringLength, right - left + 1);
   }
 
   return longestSubstringLength;
 }
 
 // 例子
-console.log(findLongestSubstring(''));                // 输出: 0
-console.log(findLongestSubstring('rithmschool'));     // 输出: 7
-console.log(findLongestSubstring('thisisawesome'));   // 输出: 6
-console.log(findLongestSubstring('thecatinthehat'));  // 输出: 7
-console.log(findLongestSubstring('bbbbbb'));           // 输出: 1
-console.log(findLongestSubstring('longestsubstring'));// 输出: 8
+// console.log(findLongestSubstring(''));                // 输出: 0
+// console.log(findLongestSubstring('rithmschool'));     // 输出: 7
+// console.log(findLongestSubstring('thisisawesome'));   // 输出: 6
+// console.log(findLongestSubstring('thecatinthehat'));  // 输出: 7
+// console.log(findLongestSubstring('bbbbbb'));           // 输出: 1
+// console.log(findLongestSubstring('longestsubstring'));// 输出: 8
 console.log(findLongestSubstring('thisishowwedoit')); // 输出: 6
 
