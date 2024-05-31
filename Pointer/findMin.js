f2b3ed58ef90d7bb53a9b7b9d2432e69108f27b0
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-05-31 15:48:47 +1000

    update

--- Pointer/findMin.js
+++ Pointer/findMin.js
@@ -9,7 +9,26 @@
 // 3 [5,6,7,0,1,2,4]
 // 4 [4,5,6,7,0,1,2]
 var findMin = function(nums) {
-    return Math.min(...nums)
+    // return Math.min(...nums)
+
+    let left = 0;
+    let right = nums.length - 1;
+    let result = nums[0];
+
+    while(left<=right) {
+        let mid = left + Math.floor((right - left) / 2);
+        // left side strictly increasing
+        if(nums[mid] >= nums[left]) {
+            result = Math.min(result, nums[left]);
+            left = mid + 1;
+        }
+        // right side strictly increasing
+        else {
+            result = Math.min(result, nums[mid]);
+            right = mid - 1;
+        }
+    }
+    return result;
 }
 
 console.log(findMin([4,5,6,7,0,1,2]))
